49c419cf91afce31cd590f3dd1ddc3814fde5773
ShemsheyMaxim <shanhaymax@gmail.com>
2021-03-02 13:00:29 +0200

    first commit

---
+++ src/test/java/com/abank/paymentportal/PaymentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abank.paymentportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
